67895c31391055551af72b84ab6699b494d4fc22
Luís Felipe <felipe.drs01@gmail.com>
2022-10-02 13:52:44 -0300

    implementacado da funcao imprimir_todos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openpyxl
+import random
 
 # carregando a planilha do Excel
 book = openpyxl.load_workbook('candidatos.xlsx')
@@ -6,6 +7,18 @@ book = openpyxl.load_workbook('candidatos.xlsx')
 # selecionando página para dep. federal
 dep_federal_page = book['dep-federal']
 
-# imprimindo todos os candidatos
-for rows in dep_federal_page.iter_rows(min_row=1, max_row=215):
-    print(rows[0].value, rows[1].value)
+
+# imprimindo todos os candidatos(separados por cargo)
+def imprimir_todos(lista_candidatos):
+    global page
+    if lista_candidatos == 'dep-federal':
+        page = book['dep-federal']
+
+    if lista_candidatos == 'presidente':
+        page = book['presidente']
+
+    for rows in page.iter_rows(min_row=1, max_row=200):
+        print(rows[0].value, rows[1].value)
+
+
+imprimir_todos('dep-federal')
