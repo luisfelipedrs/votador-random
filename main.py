5bb8424c0d94489311b52c68febbda5ec373a311
Luís Felipe <felipe.drs01@gmail.com>
2022-10-02 13:41:54 -0300

    implementacao do openpyxl para ler a planilha

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ book = openpyxl.load_workbook('candidatos.xlsx')
 
 # selecionando página para dep. federal
 dep_federal_page = book['dep-federal']
+
+# imprimindo todos os candidatos
+for rows in dep_federal_page.iter_rows(min_row=1, max_row=215):
+    print(rows[0].value, rows[1].value)
