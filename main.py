5cd24fd94ddc9894b4018fac7c299aa023420e70
Luís Felipe <felipe.drs01@gmail.com>
2022-10-02 15:12:49 -0300

    finalizacao da logica do projeto

--- main.py
+++ main.py
@@ -12,21 +12,6 @@ def imprimir_todos(lista_candidatos):
         print(rows[0].value, rows[1].value)
 
 
-# imprimir todos os candidatos a deputado federal
-def imprimir_dep_federal():
-    page = book['dep-federal']
-    for rows in page.iter_rows():
-        print(rows[0].value, rows[1].value)
-
-
-# sortear um candidato a dep. federal
-def sortear_dep_federal():
-    page = book['dep-federal']
-    random_index = random.randint(1, page.max_row)
-    for rows in page.iter_rows(min_row=random_index, max_row=random_index):
-        print(rows[0].value, rows[1].value)
-
-
 # imprimir todos os candidatos a presidente
 def imprimir_presidente():
     page = book['presidente']
@@ -42,5 +27,68 @@ def sortear_presidente():
         print(rows[0].value, rows[1].value)
 
 
+# imprimir todos os candidatos a senador
+def imprimir_senador():
+    page = book['senador']
+    for rows in page.iter_rows():
+        print(rows[0].value, rows[1].value)
+
+
+# sortear um candidato a senador
+def sortear_senador():
+    page = book['senador']
+    random_index = random.randint(1, page.max_row)
+    for rows in page.iter_rows(min_row=random_index, max_row=random_index):
+        print(rows[0].value, rows[1].value)
+
+
+# imprimir todos os candidatos a governador
+def imprimir_governador():
+    page = book['governador']
+    for rows in page.iter_rows():
+        print(rows[0].value, rows[1].value)
+
+
+# sortear um candidato a governador
+def sortear_governador():
+    page = book['governador']
+    random_index = random.randint(1, page.max_row)
+    for rows in page.iter_rows(min_row=random_index, max_row=random_index):
+        print(rows[0].value, rows[1].value)
+
+
+# imprimir todos os candidatos a deputado federal
+def imprimir_dep_federal():
+    page = book['dep-federal']
+    for rows in page.iter_rows():
+        print(rows[0].value, rows[1].value)
+
+
+# sortear um candidato a dep. federal
+def sortear_dep_federal():
+    page = book['dep-federal']
+    random_index = random.randint(1, page.max_row)
+    for rows in page.iter_rows(min_row=random_index, max_row=random_index):
+        print(rows[0].value, rows[1].value)
+
+
+# imprimir todos os candidatos a dep. distrital
+def imprimir_dep_distrital():
+    page = book['dep-distrital']
+    for rows in page.iter_rows():
+        print(rows[0].value, rows[1].value)
+
+
+# sortear um candidato a dep. distrital
+def sortear_dep_distrital():
+    page = book['dep-distrital']
+    random_index = random.randint(1, page.max_row)
+    for rows in page.iter_rows(min_row=random_index, max_row=random_index):
+        print(rows[0].value, rows[1].value)
+
+
 sortear_presidente()
+sortear_senador()
+sortear_governador()
 sortear_dep_federal()
+sortear_dep_distrital()
